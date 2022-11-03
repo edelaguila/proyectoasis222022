ed163b9ceac45ad49dadff387966f5f691930010
Joshua53Barrios <jbarrioso3@miumg.edu.gt>
2022-11-03 00:22:28 -0600

    Subiendo procesos de Bancos

---
+++ Codigo/Modulos/Bancos/Modelo_Bancos/Sentencias.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Odbc;
+namespace Modelo_Bancos
+{
+    public class Sentencias
+    {
+        Conexion con = new Conexion();
+
+        public OdbcDataAdapter llenartabla(string tabla)
+        {
+
+
+            string sql = "select * from " + tabla + ";";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, con.conexion());
+
+            return datatable;
+        }
+
+    }
+}
