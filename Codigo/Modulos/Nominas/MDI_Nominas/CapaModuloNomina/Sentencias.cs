c39b112d7ea69693837739816b84819d9c92fed0
KevinAlejandroCol8 <kevin_gonzalez16@outlook.es>
2022-11-06 16:19:13 -0600

    Asignacion puestos a Departamento

--- Codigo/Modulos/Nominas/MDI_Nominas/CapaModuloNomina/Sentencias.cs
+++ Codigo/Modulos/Nominas/MDI_Nominas/CapaModuloNomina/Sentencias.cs
@@ -19,5 +19,54 @@ namespace CapaModuloNomina
             OdbcDataAdapter dataTable = new OdbcDataAdapter(sql, con.conexion());
             return dataTable;
         }
+
+        public OdbcDataAdapter llenarListaPuesto(string tabla)
+        {
+            string sql = "select  pk_id_puesto as ID, nombre_puesto AS Puesto, estado_puesto as Estado from tbl_puestosdetrabajo where estado_puesto != 0;";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, con.conexion());
+            return datatable;
+        }
+        public OdbcDataAdapter llenarListaDepartamentos(string tabla)
+        {
+            string sql = "select pk_id_departamento as ID, nombre_departamento AS Departamento, descripcion_departamento as Descripcion from tbl_departamentos where estado_departamento != 0;";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, con.conexion());
+            return datatable;
+        }
+
+        public void insertar(string dato, string tipo, string tabla)
+        {
+            string sql = "insert into " + tabla + "(" + tipo + ") values (" + dato + ")";
+            try
+            {
+
+                OdbcCommand cmd = new OdbcCommand(sql, con.conexion());
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString() + " \nError en obtener");
+            }
+        }
+
+        public OdbcDataAdapter llenarListaAsignaciones(string tabla, string id)
+        {
+            string sql = "Select *from " + tabla + " where ID = " + id + " ;";
+            OdbcDataAdapter datatable = new OdbcDataAdapter(sql, con.conexion());
+            return datatable;
+        }
+
+        public void eliminarAsiganaciones(string tabla, string condicion1, int campo1, string condicion2, int campo2)
+        {
+            try
+            {
+                string sql = "DELETE FROM " + tabla + " where (" + condicion1 + " = " + campo1 + ") and (" + condicion2 + " =" + campo2 + ");";
+                OdbcCommand cmd = new OdbcCommand(sql, con.conexion());
+                cmd.ExecuteNonQuery();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message.ToString() + " \nNo se puede eliminar por permisos asignados");
+            }
+        }
     }
 }
