b09e443141a5096fad3f14847aa70c80fe25f1d5
Rmiranda01 <rmirandav@miumg.edu.gt>
2022-11-07 00:20:53 -0600

    formulario de ordenes de compras y conciliacion

--- Codigo/Modulos/Bancos/Vista_Bancos/Reporte_Conciliacion.Designer.cs
+++ Codigo/Modulos/Bancos/Vista_Bancos/Reporte_Conciliacion.Designer.cs
@@ -51,140 +51,170 @@ namespace Vista_Bancos
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(43, 271);
+            this.dataGridView1.Location = new System.Drawing.Point(59, 356);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(633, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(870, 197);
             this.dataGridView1.TabIndex = 29;
             // 
             // textBox7
             // 
-            this.textBox7.Location = new System.Drawing.Point(168, 168);
+            this.textBox7.Location = new System.Drawing.Point(231, 220);
+            this.textBox7.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox7.Name = "textBox7";
-            this.textBox7.Size = new System.Drawing.Size(423, 22);
+            this.textBox7.Size = new System.Drawing.Size(580, 28);
             this.textBox7.TabIndex = 28;
             // 
             // textBox6
             // 
-            this.textBox6.Location = new System.Drawing.Point(415, 225);
+            this.textBox6.Location = new System.Drawing.Point(571, 295);
+            this.textBox6.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox6.Name = "textBox6";
-            this.textBox6.Size = new System.Drawing.Size(176, 22);
+            this.textBox6.Size = new System.Drawing.Size(240, 28);
             this.textBox6.TabIndex = 27;
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(66, 168);
+            this.label7.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label7.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(91, 220);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(55, 17);
+            this.label7.Size = new System.Drawing.Size(78, 22);
             this.label7.TabIndex = 26;
             this.label7.Text = "Bancos";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(348, 225);
+            this.label6.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label6.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(478, 295);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(47, 17);
+            this.label6.Size = new System.Drawing.Size(67, 22);
             this.label6.TabIndex = 25;
             this.label6.Text = "Libros";
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(120, 222);
+            this.textBox5.Location = new System.Drawing.Point(165, 291);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(216, 22);
+            this.textBox5.Size = new System.Drawing.Size(296, 28);
             this.textBox5.TabIndex = 24;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(52, 225);
+            this.label5.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(72, 295);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(51, 17);
+            this.label5.Size = new System.Drawing.Size(74, 22);
             this.label5.TabIndex = 23;
             this.label5.Text = "Parcial";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(473, 35);
+            this.textBox4.Location = new System.Drawing.Point(650, 46);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(118, 22);
+            this.textBox4.Size = new System.Drawing.Size(161, 28);
             this.textBox4.TabIndex = 22;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(348, 38);
+            this.label4.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(478, 50);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(119, 17);
+            this.label4.Size = new System.Drawing.Size(168, 22);
             this.label4.TabIndex = 21;
             this.label4.Text = "Fecha de emision";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(120, 84);
+            this.textBox3.Location = new System.Drawing.Point(165, 110);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(216, 22);
+            this.textBox3.Size = new System.Drawing.Size(296, 28);
             this.textBox3.TabIndex = 20;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(52, 84);
+            this.label3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(72, 110);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(48, 17);
+            this.label3.Size = new System.Drawing.Size(68, 22);
             this.label3.TabIndex = 19;
             this.label3.Text = "Banco";
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(168, 126);
+            this.textBox2.Location = new System.Drawing.Point(231, 165);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(423, 22);
+            this.textBox2.Size = new System.Drawing.Size(580, 28);
             this.textBox2.TabIndex = 18;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(66, 126);
+            this.label2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(91, 165);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 17);
+            this.label2.Size = new System.Drawing.Size(119, 22);
             this.label2.TabIndex = 17;
             this.label2.Text = "Descripcion";
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(120, 38);
+            this.textBox1.Location = new System.Drawing.Point(165, 50);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(216, 22);
+            this.textBox1.Size = new System.Drawing.Size(296, 28);
             this.textBox1.TabIndex = 16;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(52, 38);
+            this.label1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(72, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(47, 17);
+            this.label1.Size = new System.Drawing.Size(67, 22);
             this.label1.TabIndex = 15;
             this.label1.Text = "Anexo";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(291, 455);
+            this.button1.Location = new System.Drawing.Point(400, 597);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.Size = new System.Drawing.Size(103, 30);
             this.button1.TabIndex = 30;
             this.button1.Text = "Generar";
             this.button1.UseVisualStyleBackColor = true;
             // 
             // Reporte_Conciliacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.LightSteelBlue;
-            this.ClientSize = new System.Drawing.Size(711, 521);
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(978, 684);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.textBox7);
@@ -201,6 +231,8 @@ namespace Vista_Bancos
             this.Controls.Add(this.label2);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Reporte_Conciliacion";
             this.Text = "Reporte_Conciliacion";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
--- Codigo/Modulos/Bancos/Vista_Bancos/Reporte_Orden.Designer.cs
+++ Codigo/Modulos/Bancos/Vista_Bancos/Reporte_Orden.Designer.cs
@@ -46,62 +46,74 @@ namespace Vista_Bancos
             // 
             // textBox5
             // 
-            this.textBox5.Location = new System.Drawing.Point(438, 129);
+            this.textBox5.Location = new System.Drawing.Point(602, 169);
+            this.textBox5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox5.Name = "textBox5";
-            this.textBox5.Size = new System.Drawing.Size(156, 22);
+            this.textBox5.Size = new System.Drawing.Size(213, 28);
             this.textBox5.TabIndex = 23;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(349, 134);
+            this.label5.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label5.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(480, 176);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(74, 17);
+            this.label5.Size = new System.Drawing.Size(105, 22);
             this.label5.TabIndex = 22;
             this.label5.Text = "Proveedor";
             // 
             // textBox4
             // 
-            this.textBox4.Location = new System.Drawing.Point(145, 129);
+            this.textBox4.Location = new System.Drawing.Point(199, 169);
+            this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox4.Name = "textBox4";
-            this.textBox4.Size = new System.Drawing.Size(185, 22);
+            this.textBox4.Size = new System.Drawing.Size(253, 28);
             this.textBox4.TabIndex = 21;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(27, 129);
+            this.label4.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label4.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(37, 169);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(59, 17);
+            this.label4.Size = new System.Drawing.Size(84, 22);
             this.label4.TabIndex = 20;
             this.label4.Text = "Recibos";
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(465, 35);
+            this.textBox3.Location = new System.Drawing.Point(639, 46);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(129, 22);
+            this.textBox3.Size = new System.Drawing.Size(176, 28);
             this.textBox3.TabIndex = 19;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(145, 81);
+            this.textBox2.Location = new System.Drawing.Point(199, 106);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(449, 22);
+            this.textBox2.Size = new System.Drawing.Size(616, 28);
             this.textBox2.TabIndex = 18;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(145, 30);
+            this.textBox1.Location = new System.Drawing.Point(199, 39);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.Size = new System.Drawing.Size(136, 28);
             this.textBox1.TabIndex = 17;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(248, 372);
+            this.button1.Location = new System.Drawing.Point(341, 488);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(142, 23);
+            this.button1.Size = new System.Drawing.Size(195, 30);
             this.button1.TabIndex = 16;
             this.button1.Text = "Mostra Reporte";
             this.button1.UseVisualStyleBackColor = true;
@@ -109,46 +121,56 @@ namespace Vista_Bancos
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(315, 35);
+            this.label3.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(433, 46);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(120, 17);
+            this.label3.Size = new System.Drawing.Size(170, 22);
             this.label3.TabIndex = 15;
             this.label3.Text = "Fecha de Emision";
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(40, 195);
+            this.dataGridView1.Location = new System.Drawing.Point(55, 256);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(554, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(762, 197);
             this.dataGridView1.TabIndex = 14;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(27, 81);
+            this.label2.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(37, 106);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(55, 17);
+            this.label2.Size = new System.Drawing.Size(82, 22);
             this.label2.TabIndex = 13;
             this.label2.Text = "Articulo";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(27, 30);
+            this.label1.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(37, 39);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(102, 17);
+            this.label1.Size = new System.Drawing.Size(144, 22);
             this.label1.TabIndex = 12;
             this.label1.Text = "Numero Orden";
             // 
             // Reporte_Orden
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 21F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkKhaki;
-            this.ClientSize = new System.Drawing.Size(640, 431);
+            this.BackColor = System.Drawing.Color.DarkTurquoise;
+            this.ClientSize = new System.Drawing.Size(880, 566);
             this.Controls.Add(this.textBox5);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.textBox4);
@@ -161,6 +183,8 @@ namespace Vista_Bancos
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Arial Rounded MT Bold", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Reporte_Orden";
             this.Text = "Reporte_Orden";
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
